d5cc32fd6a80b0509a2bc81748116e8436c00874
Andy Cavatorta <andycavatorta@gmail.com>
2018-02-17 23:05:37 -0500

    typo

--- main.py
+++ main.py
@@ -17,13 +17,19 @@ sys.path.append(UPPER_PATH)
 
 from thirtybirds_2_0.Adaptors.Actuators import stepper_pulses
 
+def left_wheel_callback():
+    print "left_wheel_callback"
+
+
+
+
 stepper_motor_channels = [
     {
         "name":"left_wheel",
         "pulse_pin":26,
         "dir_pin":6,
-        "base_pulse_period":0.001,
-        "steps_finished_callback":False,
+        "base_pulse_period":0.0005,
+        "steps_finished_callback":left_wheel_callback,
         "backwards_orientation":False,
     }
 ]
@@ -31,4 +37,7 @@ stepper_motor_channels = [
 stepper_pulses.init(stepper_motor_channels)
 
 
-
+stepper_pulses.set("left_wheel", "speed", 1.0)
+stepper_pulses.set("left_wheel", "steps", 800)
+stepper_pulses.set("left_wheel", "speed", -1.0)
+stepper_pulses.set("left_wheel", "steps", 800)
