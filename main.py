37136ac8c998b8f16ac8f91879b8b37342902026
Andy Cavatorta <andycavatorta@gmail.com>
2018-03-01 18:52:56 -0500

    updating pin numbers

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ class Coordinates_To_Vectors(object):
             return (distance, target_angle_relative_to_Cartesian_space)
         elif self.current_x > target_x and self.current_y < target_y: # somewhere in quadrant 2
             target_angle_relative_to_Cartesian_space =  90 + math.degrees(math.acos( abs(target_x-self.current_x) / distance) )
-            return (distance, target_angle_relative_to_Cartesian_space)z
+            return (distance, target_angle_relative_to_Cartesian_space)
         elif self.current_x > target_x and self.current_y > target_y: # somewhere in quadrant 3
             target_angle_relative_to_Cartesian_space =  180 + math.degrees(math.acos( abs(target_x-self.current_x) / distance) )
             return (distance, target_angle_relative_to_Cartesian_space)
