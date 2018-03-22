b15baffecd10437fd51e1fd82fd31bc68ea9f83b
Andy Cavatorta <andycavatorta@gmail.com>
2018-03-22 15:26:44 -0400

    =starting small fixes

--- Roles/bot/main.py
+++ Roles/bot/main.py
@@ -318,7 +318,9 @@ class Path_Server(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.queue = Queue.Queue()
+        print ">>>>>>>>>>>>>>>>>", network
         global network
+        print ">>>>>>>>>>>>>>>>>", network
         network.send("path_server.stroke_paths_request", True)
         self.stroke_paths = []
         self.stroke_paths_cursor = 0
