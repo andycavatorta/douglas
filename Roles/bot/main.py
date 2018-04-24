d73a3103301382e61da5c6a319e20e1717bb3367
Andy Cavatorta <andycavatorta@gmail.com>
2018-04-24 16:00:46 -0400

    imports

--- Roles/bot/main.py
+++ Roles/bot/main.py
@@ -201,15 +201,15 @@ class Motor_Control(threading.Thread):
         
         if event_type == "update":
             self.pulse_odometer[motor_name] = data 
-                if self.current_motor_command == "roll":
-                    number_of_wheel_rotations =  self.pulse_odometer[motor_name] / self.steps_per_rotation
-                    distance = number_of_wheel_rotations * self.wheel_circumference
-                    print ">>>>>>>>>>> distance", distance
-                if self.current_motor_command == "rotate":
-                    length_of_arc = (self.steps_per_rotation * 4) / pulses_of_arc
-                    proportion_of_circle = length_of_arc / self.circumference_of_rotation
-                    target_angle_relative_to_bot = proportion_of_circle *360.0
-                    print ">>>>>>>>>>> target_angle_relative_to_bot", target_angle_relative_to_bot
+            if self.current_motor_command == "roll":
+                number_of_wheel_rotations =  self.pulse_odometer[motor_name] / self.steps_per_rotation
+                distance = number_of_wheel_rotations * self.wheel_circumference
+                print ">>>>>>>>>>> distance", distance
+            if self.current_motor_command == "rotate":
+                length_of_arc = (self.steps_per_rotation * 4) / pulses_of_arc
+                proportion_of_circle = length_of_arc / self.circumference_of_rotation
+                target_angle_relative_to_bot = proportion_of_circle *360.0
+                print ">>>>>>>>>>> target_angle_relative_to_bot", target_angle_relative_to_bot
             self.external_callback(motor_name, self.current_motor_command, self.pulse_odometer[motor_name]) # TO DO: ADD PARAMETERS
         if event_type == "finished":
             self.finished[motor_name] = True 
